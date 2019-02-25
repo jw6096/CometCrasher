a03137ecf5ec3e80f137ae6e40a541081d1be45b
LoganL <ll8875@g.rit.edu>
2019-02-25 10:36:27 -0500

    Dummy Game Manager prefab to reference the singleton

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Dummy script that handles UI and refers scene management calls to the GameManager singleton
+/// </summary>
+public class UIHandler : MonoBehaviour
+{
+
+    GameManager gMan;
+
+    public GameManager GameManagerInstance
+    {
+        get { return gMan; }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        gMan = GameManager.instance;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void UnlockLevel(int level)
+    {
+        GameManager.instance.UnlockLevel(level);
+    }
+
+    public void NextLevel()
+    {
+        GameManager.instance.NextLevel();
+    }
+
+    public void LoadLevel(int level)
+    {
+        GameManager.instance.LoadLevel(level);
+    }
+    public void LoadLevel(string levelName)
+    {
+        GameManager.instance.LoadLevel(levelName);
+    }
+    public void LoadCurrentLevel()
+    {
+        GameManager.instance.LoadCurrentLevel();
+    }
+}
