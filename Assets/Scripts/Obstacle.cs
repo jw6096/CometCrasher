127cbbebbec212d2b62f17519ea7b37a42522a4d
LoganL <ll8875@g.rit.edu>
2019-04-17 10:54:41 -0400

    Started working on moving obstacles

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,16 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle : MonoBehaviour {
+public class Obstacle : MonoBehaviour
+{
+    public bool allowMovement = false;
+    public float moveForce;
+    public int horizontalRange = 0;
+    public int verticalRange = 0;
+
+    Vector3 startPos;
+    Rigidbody2D rb;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        startPos = transform.position;
+
+        if(allowMovement)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(allowMovement)
+        {
+            if(horizontalRange != 0)
+            {
+                if (Mathf.Abs(transform.position.x - startPos.x) >= Mathf.Abs(horizontalRange))
+                {
+                    transform.position = new Vector3(startPos.x + horizontalRange, startPos.y, startPos.z);
+                    rb.velocity = Vector2.zero;
+                }
+            }
+            else if (verticalRange != 0)
+            {
+                if (Mathf.Abs(transform.position.y - startPos.y) >= Mathf.Abs(verticalRange))
+                {
+                    transform.position = new Vector3(startPos.x, startPos.y + verticalRange, startPos.z);
+                    rb.velocity = Vector2.zero;
+                }
+            }
+        }
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
