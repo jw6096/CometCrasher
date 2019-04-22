c4af97f533dafa94a3184bb484dc30b613266433
Eleanor Post (RIT Student) <eap9238@ad.rit.edu>
2019-04-22 10:24:33 -0400

    camera movement thing?

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = transform.parent;
+
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Vector3.Magnitude(player.localPosition - transform.localPosition) )
+
+        transform.localPosition += new Vector3(player.localPosition.x - transform.localPosition.x, player.localPosition.y - transform.localPosition.y, 0) * Time.deltaTime;
+    }
+}
