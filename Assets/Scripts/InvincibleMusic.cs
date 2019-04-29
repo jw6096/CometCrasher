b39d50a4b1f6a8f69d50bee742f03df1ebffc90c
Joe Wang (RIT Student) <jxw6096@ad.rit.edu>
2019-04-29 10:22:14 -0400

    Update InvincibleMusic.cs
    updated music so it doesn't kill ears

--- Assets/Scripts/InvincibleMusic.cs
+++ Assets/Scripts/InvincibleMusic.cs
@@ -1,17 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InvincibleMusic : MonoBehaviour
 {
     void Awake()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
-
-        if (objs.Length > 1)
-            Destroy(this.gameObject);
-
-        DontDestroyOnLoad(this.gameObject);
+        
     }
     // Start is called before the first frame update
     void Start()
@@ -22,6 +18,21 @@ public class InvincibleMusic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        string sceneName = SceneManager.GetActiveScene().name;
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
 
+        if (sceneName != "MainMenu")
+        {
+            if (objs.Length > 1)
+                Destroy(this.gameObject);
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Object.Destroy(this.gameObject);
+        }
+
+        Debug.Log(sceneName);
     }
 }
